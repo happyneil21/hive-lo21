c77a770223b8aa6878ee05e81c6df46e6c9f2ed9
Massplay <148557117+Massplay@users.noreply.github.com>
2024-12-07 18:35:27 +0100

    Update Game.h

--- HIVE LO21 - JEU/Game.h
+++ HIVE LO21 - JEU/Game.h
@@ -17,18 +17,31 @@ public:
     }
 
     void ajouterPiece(Piece* piece, int x, int y);
-    void deplacerPiece(int index, int x, int y);
+    void deplacerPiece(Piece* piece, int x, int y);
     bool GameOver() const;
     bool TouteslesCasessontConnectées(const vector<Case>& plateau) const;
 
 private:
     Jeu(); //constructeur privé pour le Singleton
     ~Jeu() {}
-    Jeu(const Jeu&) = delete; 
+    Jeu(const Jeu&) = delete;
     Jeu& operator=(const Jeu&) = delete;
     static Jeu* instance; // Pointeur statique vers l'instance unique
-    vector<Piece*> pieces_placees;
-    vector <Piece*> pieces_disponible;
-    vector <Case*> plateau;
+    vector<Piece*> pieces_disponible;
+    vector<Case> plateau; // Note: vector<Case> instead of vector<Case*>
     bool verifConditionVictoire() const;
+};
+
+class Joueur {
+public:
+
+private:
+    string pseudo;
+
+};
+
+class Controleur {
+public:
+
+private:
 };

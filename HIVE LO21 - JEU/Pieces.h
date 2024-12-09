110c23f6de9627f75bb1e31343cce6552ed4b95c
ferminator375 <96304892+ferminator375@users.noreply.github.com>
2024-12-09 14:26:19 +0100

    Update Pieces.h
    adaptation du code au nouveau système de coordonnée

--- HIVE LO21 - JEU/Pieces.h
+++ HIVE LO21 - JEU/Pieces.h
@@ -9,54 +9,56 @@ using namespace std;
 
 class Piece {
 public:
-    Piece(const string& type) : type(type), x(0), y(0) {} // On construit une pièce uniquement à partir de son type en supposant qu'elle n'est pas sur le plateau dès qu'elle est créée
+    Piece(const string& type) : type(type), place(false), q(0), r(0), s(0) {} // On construit une pièce uniquement à partir de son type en supposant qu'elle n'est pas sur le plateau dès qu'elle est créée
     virtual ~Piece() {}
 
-    int getX() const { return x; }
-    int getY() const { return y; }
+    int getQ() const { return q; }
+    int getR() const { return r; }
+    int getS() const { return s; }
     string getType() const { return type; }
     bool estPlacee() const { return placee; }
-    void placerPiece(int x, int y);
-    void deplacerPiece(int x, int y);
+    void placerPiece(int q, int r, int s);
+    void deplacerPiece(int q, int r, int s);
 
 protected: // Pour donner accès à ces données dans les classes qui héritent de Piece
-    int x, y; // Coordonnées pour l'attribut position
+    int q, r, s; // Coordonnées pour l'attribut position
     string type; // Type de la pièce
     bool placee = false; // Pour savoir si la pièce créée est en jeu ou non
 };
 
 class Case {
 public:
-    Case(int x, int y) : m_x(x), m_y(y), m_piece(nullptr) {}
+    Case(int q, int y) : m_q(q), m_r(r), m_s(s), m_piece(nullptr) {} // Une case à par défault pas de pièce placée dessu
 
-    int getX() const { return m_x; }
-    int getY() const { return m_y; }
+    int getQ() const { return m_q; }
+    int getR() const { return m_r; }
+    int getS() const { return m_s; }
     bool estOccupe() const { return m_piece != nullptr; }
     Piece* getPiece() const { return m_piece; }
     void setPiece(Piece* piece) { m_piece = piece; }
     bool estAdjacente(const vector<Case>& plateau) const;
 
 private:
-    int m_x, m_y;
+    int m_q, m_r, m_s;
     Piece* m_piece;
 };
 
 class ReineAbeille : public Piece {
 public:
     ReineAbeille() : Piece("Reine Abeille") {}
-    void deplacerReine(int x, int y, const vector<Case>& plateau);// vecteur qui va prendre en mémoire touts les objets cases du plateau
+    void deplacerReine(int q, int r, int s, const vector<Case>& plateau);// vecteur qui va prendre en mémoire touts les objets cases du plateau
     bool reineEntourer(const vector<Case>& plateau) const;
 private:
-    bool estOcupe(int x, int y, const vector<Case>& plateau) const;
+    bool estOcupe(int q, int r, int s, const vector<Case>& plateau) const;
 };
 
 class Araignee : public Piece {
 public:
     Araignee() : Piece("Araignée") {}
-    void deplacerAraignee(int x, int y, const vector<Case>& plateau);
+    void deplacerAraignee(int q, int r, int s, const vector<Case>& plateau);
 private:
-    bool estAccessibleEn3cases(int x, int y, const vector<Case>& plateau) const;
-    bool estOcupe(int x, int y, const vector<Case>& plateau) const;
+    bool estAccessibleEn3cases(int q, int r, int s, const vector<Case>& plateau) const;
+    bool estOcupe(int q, int r, int s, const vector<Case>& plateau) const;
 };
 
 class Scarabee : public Piece {
